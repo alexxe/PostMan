78f1cda5e2d7caead46072bcb2d8d620e665f937
Dimitry <d.levine@gmx.net>
2019-04-30 13:10:10 +0200

    add cors

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const typeDefs = importSchema('./schema.graphql');
 const passport = require('passport');
 const auth = require('./auth');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 async function main() {
   const binding = await db();
@@ -28,10 +29,21 @@ async function main() {
       db: binding
     })
   });
+
+  // server.express.options(
+  //   '*',
+  //   cors({
+  //     origin: false
+  //   })
+  // );
+  // server.express.use(
+  //   cors({
+  //     origin: false
+  //   })
+  // );
   server.express.use(bodyParser.json());
   server.express.use(passport.initialize());
   server.express.use('/auth', auth);
-
   server.express.use('/graphql', (req, res, next) => {
     passport.authenticate(
       'jwt',
@@ -54,8 +66,8 @@ async function main() {
   server.start(
     {
       cors: {
-        credentials: true,
-        origin: process.env.FRONTEND_URL
+        origin: '*',
+        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE']
       },
       endpoint: '/graphql',
       playground: '/playground',
